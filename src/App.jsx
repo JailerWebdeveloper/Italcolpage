09eea75fcbaf69a99b1e67db4d31fac789719b97
Jailerwebwork <jailerwebwork@gmail.com>
2024-01-27 17:57:35 -0500

    arreglado ruta inbox

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Fragment } from "react";
 import { BrowserRouter,Route,Routes ,Link, Navigate } from "react-router-dom";
 import Dashboard from "./Pages/Dashboard.jsx";
 import "./App.css";
-import Inbox from "./Pages/subpages/Inbox/inbox.jsx";
+import Inbox from "../src/Pages/subpages/Inbox/Inbox.jsx";
 import Dashmenu from "./Pages/subpages/Inbox/Dashmenu.jsx";
 import Agenda from "./Pages/subpages/Agenda.jsx";
 function App() {  
